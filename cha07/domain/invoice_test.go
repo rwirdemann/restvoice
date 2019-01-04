75d0654e1538f739ec85a8adf0700a2adbce5ab5
Ralf Wirdemann <ralf.wirdemann@gmail.com>
2019-01-04 15:25:37 +0100

    Adds invoice test

---
+++ cha07/domain/invoice_test.go
@@ -0,0 +1,30 @@
+package domain
+
+import (
+	"testing"
+
+	"github.com/rwirdemann/restvoice/cha05/domain"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAddPosition(t *testing.T) {
+	// Setup
+	i := domain.Invoice{}
+
+	// Run
+	i.AddPosition(1, "Programmierung", 20, 60)
+	i.AddPosition(1, "Programmierung", 12, 60)
+	i.AddPosition(1, "Qualitätssicherung", 3, 55)
+	i.AddPosition(2, "Projektmanagement", 24, 50)
+	i.AddPosition(2, "Qualitätssicherung", 8, 55)
+
+	// Assert
+	expected := domain.Position{Hours: 32, Price: 1920}
+	assert.Equal(t, expected, i.Positions[1]["Programmierung"])
+	expected = domain.Position{Hours: 3, Price: 165}
+	assert.Equal(t, expected, i.Positions[1]["Qualitätssicherung"])
+	expected = domain.Position{Hours: 24, Price: 1200}
+	assert.Equal(t, expected, i.Positions[2]["Projektmanagement"])
+	expected = domain.Position{Hours: 8, Price: 440}
+	assert.Equal(t, expected, i.Positions[2]["Qualitätssicherung"])
+}
